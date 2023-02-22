43b1098b1ba210067ecc81f24b8aab5025f9e987
Neil Massey <neil.massey@stfc.ac.uk>
2023-02-22 15:47:35 +0000

    Complete set of test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,21 +27,26 @@ class test_data:
 
     def make_data(self):
         # generate the test data using Python.  The shell script is redundant.
-        top_path = pathlib.Path(__file__).parent.parent.resolve().as_posix()
+        top_path = pathlib.Path(__file__).parent.parent.resolve()
+        # check the data directory exists
+        datadir = top_path.joinpath("data")
+        if not datadir.exists():
+            datadir.mkdir()
         # create the readable files (user can read)
         for i in range(1, self.nr+1):
-            path = pathlib.Path(top_path).joinpath(f"data/readable_file_{i}.txt")
+            path = top_path.joinpath(f"data/readable_file_{i}.txt")
             with path.open("w") as fh:
                 fh.write("!")
                 # write 128K of random into the file
         # create the unreadable files (user does not have permission to read)
         for i in range(1, self.ur+1):
-            path = pathlib.Path(top_path).joinpath(f"data/unreadable_file_{i}.txt")
+            path = top_path.joinpath(f"data/unreadable_file_{i}.txt")
             with path.open("w") as fh:
                 fh.write("!")
                 # write 128K of random into the file
             # change the file permissions
             path.chmod(0o000)
+
 
 @pytest.fixture()
 def data_fixture():
@@ -53,6 +58,7 @@ def data_fixture():
         data.make_data()
     yield setup
     data.del_data()
+
 
 @pytest.fixture()
 def catalog_fixture():
@@ -71,6 +77,7 @@ def catalog_fixture():
     yield
     p.terminate()
 
+
 @pytest.fixture()
 def monitor_fixture():
     # run the monitor executable
@@ -85,5 +92,66 @@ def monitor_fixture():
         os.unlink(db_name)
     
     p = subprocess.Popen(["monitor_q"])
+    yield
+    p.terminate()
+
+
+@pytest.fixture()
+def index_fixture():
+    # run the indexer executable
+    # this requires NLDS to be pip install and the `index_q` command to be
+    # available in the path
+    p = subprocess.Popen(["index_q"])
+    yield
+    p.terminate()
+
+
+@pytest.fixture()
+def worker_fixture():
+    # run the worker executable
+    # this requires NLDS to be pip install and the `nlds_q` command to be
+    # available in the path
+    p = subprocess.Popen(["nlds_q"])
+    yield
+    p.terminate()
+
+
+@pytest.fixture()
+def server_fixture():
+    # run the HTTP rest server executable
+    # this requires uvicorn to be installed and the `uvicorn` command to be
+    # available on the path
+    p = subprocess.Popen([
+        "uvicorn", "nlds.main:nlds", "--reload","--log-level=trace", "--port=8000"
+    ])
+    yield
+    p.terminate()
+
+
+@pytest.fixture()
+def put_transfer_fixture():
+    # run the put_transfer executable
+    # this requires NLDS to be pip install and the `put_transfer_q` command to 
+    # be available in the path
+    p = subprocess.Popen(["transfer_put_q"])
+    yield
+    p.terminate()
+
+@pytest.fixture()
+def get_transfer_fixture():
+    # run the get_transfer executable
+    # this requires NLDS to be pip install and the `get_transfer_q` command to 
+    # be available in the path
+    p = subprocess.Popen(["transfer_get_q"])
+    yield
+    p.terminate()
+
+
+@pytest.fixture()
+def logger_fixture():
+    # run the logging executable
+    # this requires NLDS to be pip install and the `logging_q` command to 
+    # be available in the path
+    p = subprocess.Popen(["logging_q"])
     yield
     p.terminate()
--- tests/test_put.py
+++ tests/test_put.py
@@ -1,11 +1,15 @@
 from nlds_client import nlds_client
 import time
+import pytest
 
-# def test_put(catalog_fixture, monitor_fixture, data_fixture):
-#     time.sleep(30)
-#     print("TEST TEST TEST")
+@pytest.mark.usefixtures("data_fixture", "catalog_fixture", "monitor_fixture", 
+    "index_fixture", "worker_fixture", "server_fixture", "put_transfer_fixture",
+    "get_transfer_fixture", "logger_fixture")
+class TestPut:
 
-def test_put(data_fixture):
-    data = data_fixture(5, 5)
-    time.sleep(3)
-    print("TEST TEST TEST")
+    def test_put(self, data_fixture, catalog_fixture, monitor_fixture, 
+                 index_fixture, worker_fixture, server_fixture, 
+                 put_transfer_fixture, get_transfer_fixture, logger_fixture):
+        data = data_fixture(5, 5)
+        time.sleep(30)
+        print("TEST TEST TEST")
